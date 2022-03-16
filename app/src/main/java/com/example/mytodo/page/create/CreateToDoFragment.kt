4ab3766d70164a0075f474311b94017fb59a890d
yoshipjr <yoshihisa4413kitahara@gmail.com>
2022-03-16 21:54:56 +0900

    get view model”

--- app/src/main/java/com/example/mytodo/page/create/CreateToDoFragment.kt
+++ app/src/main/java/com/example/mytodo/page/create/CreateToDoFragment.kt
@@ -1,7 +1,11 @@
 package com.example.mytodo.page.create
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import com.example.mytodo.MainViewModel
 import com.example.mytodo.R
 
 class CreateToDoFragment: Fragment(R.layout.create_todo_fragment) {
+    private val vm: MainViewModel by viewModels()
 }
--- app/src/main/java/com/example/mytodo/page/detail/ToDoDetailFragment.kt
+++ app/src/main/java/com/example/mytodo/page/detail/ToDoDetailFragment.kt
@@ -1,7 +1,9 @@
 package com.example.mytodo.page.detail
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.mytodo.R
 
 class ToDoDetailFragment: Fragment(R.layout.todo_detail_fragment) {
+    private val vm: ToDoDetailViewModel by viewModels()
 }
